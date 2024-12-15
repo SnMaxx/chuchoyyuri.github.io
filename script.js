d95ca992408b2672841e4200d126d38355ef9303
ChuchoYYuri <call.floor.hill@usecloaked.com>
2024-12-14 21:20:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-<div id="flipbook">
-	<div class="hard"> Turn.js </div>
-	<div class="hard"></div>
-	<div> Page 1 </div>
-	<div> Page 2 </div>
-	<div> Page 3 </div>
-	<div> Page 4 </div>
-	<div class="hard"></div>
-	<div class="hard"></div>
-</div>
-<script type="text/javascript">
-	$("#flipbook").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
-	});
-</script>
+var p = new Peel('#book');
+p.setMode('book');
+p.handleDrag(function(evt, x, y) {
+  this.setPeelPosition(x, y);
+});
