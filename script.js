6486b17024c039d471151aef7bb03f489b400c35
ChuchoYYuri <call.floor.hill@usecloaked.com>
2024-12-14 19:54:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-// Inicializar Turn.js
-$(document).ready(function() {
-    $('.flipbook-container').turn({
-        width: 800, // Ancho del flipbook
-        height: 600, // Altura del flipbook
-        autoCenter: true, // Centrar automáticamente
-        duration: 1000, // Duración del giro
-    });
-});
+<div id="flipbook">
+	<div class="hard"> Turn.js </div>
+	<div class="hard"></div>
+	<div> Page 1 </div>
+	<div> Page 2 </div>
+	<div> Page 3 </div>
+	<div> Page 4 </div>
+	<div class="hard"></div>
+	<div class="hard"></div>
+</div>
+<script type="text/javascript">
+	$("#flipbook").turn({
+		width: 400,
+		height: 300,
+		autoCenter: true
+	});
+</script>
