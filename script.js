ab282dd57429e130ee5d0e2f2ddbd53a451f9823
ChuchoYYuri <call.floor.hill@usecloaked.com>
2024-12-14 19:29:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-// Selecciona todas las páginas
-const pages = document.querySelectorAll('.page');
-let currentPage = 0;
-
-// Inicializar: ocultar todas las páginas excepto la primera
-pages.forEach((page, index) => {
-    if (index !== 0) {
-        page.classList.add('hidden');
-    }
-});
-
-// Evento de clic para cambiar de página
-document.addEventListener('click', () => {
-    if (currentPage < pages.length - 1) {
-        // Oculta la página actual
-        pages[currentPage].classList.add('hidden');
-
-        // Muestra la siguiente página
-        currentPage++;
-        pages[currentPage].classList.remove('hidden');
-    } else {
-        // Reinicia al principio
-        pages[currentPage].classList.add('hidden');
-        currentPage = 0;
-        pages[currentPage].classList.remove('hidden');
-    }
+// Inicializar Turn.js
+$(document).ready(function() {
+    $('.flipbook-container').turn({
+        width: 800, // Ancho del flipbook
+        height: 600, // Altura del flipbook
+        autoCenter: true, // Centrar automáticamente
+        duration: 1000, // Duración del giro
+    });
 });
